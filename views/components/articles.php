f5e3d5468297547b9d5ee7474960e4a17fe5df28
antonpv1992 <antonpv1992@email.ua>
2021-05-10 20:37:07 +0300

    completed the article component

--- views/components/articles.php
+++ views/components/articles.php
@@ -1,30 +1,31 @@
 <main class="main">
+  <?php if(!empty($db)):
+    foreach($db as $article):
+  ?>
   <article class="main__post post">
     <div class="post__box">
       <header class="post__header">
         <section class="post__header-title">
           <h2 class="post__title">
-            <a class="post__title-link" href="" >Название</a>
+            <a class="post__title-link" href="" ><?php echo $article['name']?></a>
           </h2>
           <p class="post__title-description">
-            Описание Описание Описание Описание Описание Описание Описание
+              <?php echo $article['descriprion']?>
           </p>
         </section>
         <section class="post__meta">
-          <time class="post__meta-published" datetime="2021-04-15">15.04.2021</time>
+          <time class="post__meta-published" datetime="<?php echo implode('-', array_reverse(explode('.',$article['date'])));?>"><?php echo $article['date']?></time>
           <a class="post__meta-author" href="">
-            <span class="post__meta-name">Иван Иванов</span>
-            <img class="post__meta-image" src="https://avatarko.ru/img/kartinka/28/multfilm_minion_banan_27649.jpg" alt="Картинка">
+            <span class="post__meta-name"><?php echo $article['author']?></span>
+            <img class="post__meta-image" src="<?php echo $article['avatar']?>" alt="Картинка">
           </a>
         </section>
       </header>
       <a class="post__image-link" href="">
-        <img class="post__image" src="https://loremflickr.com/755/306" alt="Картинка">
+        <img class="post__image" src="<?php echo $article['image']?>" alt="Картинка">
       </a>
       <p class="post__descripton">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga numquam quidem facere error beatae sunt, harum blanditiis, sit a nostrum qui aliquid possimus odio soluta voluptas porro reprehenderit quo exercitationem.
-        Dolores ut earum corporis adipisci saepe veritatis! Ex, totam pariatur? Quam dolorem eum ipsa quo nam laborum alias blanditiis vero voluptate, soluta dicta repellendus, sint, repellat similique enim expedita perferendis!
-        Error minima nulla vel. Explicabo nemo, velit quasi aut, nisi vero itaque consectetur vitae suscipit totam aliquid ad repellendus hic perspiciatis ducimus qui ab quis delectus incidunt iusto! Numquam, natus.
+          <?php echo $article["short-post"] ?>
       </p>
       <footer class="post__footer">
         <a class="post__read">
@@ -32,152 +33,21 @@
         </a>
         <ul class="post__stats">
           <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-circle php-icon"></i>Тема PHP</a>
+            <a class="post__stats-link" href=""><i class="fas fa-circle <?php echo strtolower($article['theme'])?>-icon"></i><?php echo $article['theme']?></a>
           </li>
           <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-heart"></i>Лайки</a>
+            <a class="post__stats-link" href=""><i class="fas fa-heart <?php echo $article['liked'] ? 'post__stats-like' : ''?>"></i><?php echo $article['likes']?></a>
           </li>
           <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-comments"></i>Комментарии</a>
+            <a class="post__stats-link" href=""><i class="fas fa-comments <?php echo $article['commented'] ? 'post__stats-comment' : ''?>"></i><?php echo $article['comments']?></a>
           </li>
         </ul>
       </footer>
     </div>
   </article>
-  <article class="main__post post">
-    <div class="post__box">
-      <header class="post__header">
-        <section class="post__header-title">
-          <h2 class="post__title">
-            <a class="post__title-link" href="" >Название</a>
-          </h2>
-          <p class="post__title-description">
-            Описание Описание Описание Описание Описание Описание Описание
-          </p>
-        </section>
-        <section class="post__meta">
-          <time class="post__meta-published" datetime="2021-04-15">15.04.2021</time>
-          <a class="post__meta-author" href="">
-            <span class="post__meta-name">Иван Иванов</span>
-            <img class="post__meta-image" src="https://avatarko.ru/img/kartinka/28/multfilm_minion_banan_27649.jpg" alt="Картинка">
-          </a>
-        </section>
-      </header>
-      <a class="post__image-link" href="">
-        <img class="post__image" src="https://loremflickr.com/755/306" alt="Картинка">
-      </a>
-      <p class="post__descripton">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga numquam quidem facere error beatae sunt, harum blanditiis, sit a nostrum qui aliquid possimus odio soluta voluptas porro reprehenderit quo exercitationem.
-        Dolores ut earum corporis adipisci saepe veritatis! Ex, totam pariatur? Quam dolorem eum ipsa quo nam laborum alias blanditiis vero voluptate, soluta dicta repellendus, sint, repellat similique enim expedita perferendis!
-        Error minima nulla vel. Explicabo nemo, velit quasi aut, nisi vero itaque consectetur vitae suscipit totam aliquid ad repellendus hic perspiciatis ducimus qui ab quis delectus incidunt iusto! Numquam, natus.
-      </p>
-      <footer class="post__footer">
-        <a class="post__read">
-          Читать далее
-        </a>
-        <ul class="post__stats">
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-circle js-icon"></i>Тема JS</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-heart"></i>Лайки</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-comments"></i>Комментарии</a>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  </article>
-  <article class="main__post post">
-    <div class="post__box">
-      <header class="post__header">
-        <section class="post__header-title">
-          <h2 class="post__title">
-            <a class="post__title-link" href="" >Название</a>
-          </h2>
-          <p class="post__title-description">
-            Описание Описание Описание Описание Описание Описание Описание
-          </p>
-        </section>
-        <section class="post__meta">
-          <time class="post__meta-published" datetime="2021-04-15">15.04.2021</time>
-          <a class="post__meta-author" href="">
-            <span class="post__meta-name">Иван Иванов</span>
-            <img class="post__meta-image" src="https://avatarko.ru/img/kartinka/28/multfilm_minion_banan_27649.jpg" alt="Картинка">
-          </a>
-        </section>
-      </header>
-      <a class="post__image-link" href="">
-        <img class="post__image" src="https://loremflickr.com/755/306" alt="Картинка">
-      </a>
-      <p class="post__descripton">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga numquam quidem facere error beatae sunt, harum blanditiis, sit a nostrum qui aliquid possimus odio soluta voluptas porro reprehenderit quo exercitationem.
-        Dolores ut earum corporis adipisci saepe veritatis! Ex, totam pariatur? Quam dolorem eum ipsa quo nam laborum alias blanditiis vero voluptate, soluta dicta repellendus, sint, repellat similique enim expedita perferendis!
-        Error minima nulla vel. Explicabo nemo, velit quasi aut, nisi vero itaque consectetur vitae suscipit totam aliquid ad repellendus hic perspiciatis ducimus qui ab quis delectus incidunt iusto! Numquam, natus.
-      </p>
-      <footer class="post__footer">
-        <a class="post__read">
-          Читать далее
-        </a>
-        <ul class="post__stats">
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-circle css-icon"></i>Тема CSS</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-heart"></i>Лайки</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-comments"></i>Комментарии</a>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  </article>
-  <article class="main__post post">
-    <div class="post__box">
-      <header class="post__header">
-        <section class="post__header-title">
-          <h2 class="post__title">
-            <a class="post__title-link" href="" >Название</a>
-          </h2>
-          <p class="post__title-description">
-            Описание Описание Описание Описание Описание Описание Описание
-          </p>
-        </section>
-        <section class="post__meta">
-          <time class="post__meta-published" datetime="2021-04-15">15.04.2021</time>
-          <a class="post__meta-author" href="">
-            <span class="post__meta-name">Иван Иванов</span>
-            <img class="post__meta-image" src="https://avatarko.ru/img/kartinka/28/multfilm_minion_banan_27649.jpg" alt="Картинка">
-          </a>
-        </section>
-      </header>
-      <a class="post__image-link" href="">
-        <img class="post__image" src="https://loremflickr.com/755/306" alt="Картинка">
-      </a>
-      <p class="post__descripton">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga numquam quidem facere error beatae sunt, harum blanditiis, sit a nostrum qui aliquid possimus odio soluta voluptas porro reprehenderit quo exercitationem.
-        Dolores ut earum corporis adipisci saepe veritatis! Ex, totam pariatur? Quam dolorem eum ipsa quo nam laborum alias blanditiis vero voluptate, soluta dicta repellendus, sint, repellat similique enim expedita perferendis!
-        Error minima nulla vel. Explicabo nemo, velit quasi aut, nisi vero itaque consectetur vitae suscipit totam aliquid ad repellendus hic perspiciatis ducimus qui ab quis delectus incidunt iusto! Numquam, natus.
-      </p>
-      <footer class="post__footer">
-        <a class="post__read">
-          Читать далее
-        </a>
-        <ul class="post__stats">
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-circle html-icon"></i>Тема HTML</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-heart"></i>Лайки</a>
-          </li>
-          <li class="post__stats-item">
-            <a class="post__stats-link" href=""><i class="fas fa-comments"></i>Комментарии</a>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  </article>
+  <?php
+    endforeach;
+    endif;
+  ?>
   <?php include_once './views/components/pagination.php'?>
 </main>
